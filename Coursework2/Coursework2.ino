48be8e128d6f91de70f8daac4b3b399fd569ed68
Rory Paterson <rorypaterson@138-38-234-8.resnet.bath.ac.uk>
2025-03-31 16:34:47 +0100

    basic setup

--- Coursework2/Coursework2.ino
+++ Coursework2/Coursework2.ino
@@ -8,7 +8,7 @@ int motionSensorButton;
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
- 
+
 void setup() {
   
   attachInterrupt(0, tempSensorOverride, FALLING);
