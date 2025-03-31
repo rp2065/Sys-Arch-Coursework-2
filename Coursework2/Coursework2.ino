f71efc25c4067ece66dc24772ceda4065f3d9d5d
rp2065 <rp2065@bath.ac.uk>
2025-03-31 15:15:53 +0100

    Add files via upload

---
+++ Coursework2/Coursework2.ino
@@ -0,0 +1,20 @@
+void setup() {
+  
+  attachInterrupt(0, tempSensorOverride, RISING);
+  attachInterrupt(1, motionSensorOverride, RISING);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // testing git functionality
+}
+
+
+void tempSensorOverride(){
+  hello hello hello
+}
+
+void motionSensorOverride(){
+  
+}
